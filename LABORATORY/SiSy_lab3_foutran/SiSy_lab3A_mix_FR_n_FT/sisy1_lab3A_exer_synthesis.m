6a04e2fa05d5a9373a470fef1b67a4eacf914469
Marina Tavares <dqtm@zhaw.ch>
2018-11-01 14:37:35 +0100

    few notation improvements

--- LABORATORY/SiSy_lab3_foutran/SiSy_lab3A_mix_FR_n_FT/sisy1_lab3A_exer_synthesis.m
+++ LABORATORY/SiSy_lab3_foutran/SiSy_lab3A_mix_FR_n_FT/sisy1_lab3A_exer_synthesis.m
@@ -8,12 +8,12 @@ Ts = 1/Fs;    % s
 
 % Load and hear to reference sound
 load Glas
-sound(y,fs)
+sound(y_t,fs)
 
 % Analyse reference sound with DFT
 N =     1;  % Bitte ueberschreiben
-Y =     1;  % Bitte ueberschreiben
-Y_dB =  1;  % Bitte ueberschreiben
+Y_f =     1;  % Bitte ueberschreiben
+Y_f_dB =  1;  % Bitte ueberschreiben
 
 % Declare aux, time and freq vectors
 aux =   1;  % Bitte ueberschreiben
@@ -22,13 +22,13 @@ t =     1;  % Bitte ueberschreiben
 
 % Plot Reference Sound in time & freq domains
 figure(1)
-plot(t,y),grid;
+plot(t,y_t),grid;
 
 figure(2)
-plot(f,Y_dB),grid;
+plot(f,Y_f_dB),grid;
 disp('Pause before zooming in on peaks')
 pause(3)
-axis([0 Fs/2 max(Y_dB)-30 max(Y_dB)+2])
+axis([0 Fs/2 max(Y_f_dB)-30 max(Y_f_dB)+2])
 
 %%
 % Synthesize sound with Sum of sinus * envelope curve
@@ -38,17 +38,17 @@ axis([0 Fs/2 max(Y_dB)-30 max(Y_dB)+2])
 % ....  % Bitte ueberschreiben 
 
 % Sum up sinus-signals (each with respective frequency & amplitude)
-x=zeros(1,N);    % Bitte ueberschreiben 
+x_t = zeros(1,N);    % Bitte ueberschreiben 
  
 
 % Point-Multiply (gewichten) the sum of sinus with envelope curve
-x=zeros(1,N);    % Bitte ueberschreiben 
+x_t = zeros(1,N);    % Bitte ueberschreiben 
 
 
 % Play Synthesised Sound and Replay original for comparison
-sound(x',Fs);
+sound(x_t',Fs);
 pause(2)
-sound(y,Fs)
+sound(y_t,Fs)
 
 
 % check your envelope curve by superposing it to figure 1
